651dfa1f46411967f1885c26d89cc65ccd76db55
fdmsac202407-rgb <fdmsac202407@gmail.com>
2026-06-10 12:18:04 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,23 @@
-self.addEventListener("install", function(event){
-  console.log("Service Worker Installed");
+const CACHE_NAME = "swad-billing-v1";
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./style.css",
+        "./script.js",
+        "./manifest.json"
+      ]);
+    })
+  );
 });
 
-self.addEventListener("fetch", function(event){
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
 });
